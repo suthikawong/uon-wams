6453c4aadd0495bc7c873424b741d917eea545d4
suthikawong <suthika.wsrd@gmail.com>
2024-11-20 20:42:32 +0000

    add login feature

---
+++ src/main/java/com/uon/uonwams/models/Authentication.java
@@ -0,0 +1,41 @@
+package com.uon.uonwams.models;
+
+import com.password4j.BcryptFunction;
+import com.password4j.Hash;
+import com.password4j.Password;
+import com.password4j.types.Bcrypt;
+
+import java.util.List;
+
+public class Authentication {
+    private final List<User> userData;
+
+    private static final BcryptFunction bcrypt = BcryptFunction.getInstance(Bcrypt.B, 12);
+
+    public Authentication(List<User> users) {
+        this.userData = users;
+    }
+
+    public User login(int userId, String password) {
+        for (User user: userData) {
+            if (userId == user.getUserId() & isMatchedPassword(password, user.getPassword())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private static String hashPassword(String password) {
+        // Ref: https://davidbertoldi.medium.com/hashing-passwords-in-java-757e787ce71c
+        Hash hash = Password.hash(password)
+                .addPepper("shared-secret")
+                .with(bcrypt);
+        return hash.getResult();
+    }
+
+    private boolean isMatchedPassword(String plainPassword, String storedPassword) {
+        return Password.check(plainPassword, storedPassword)
+                .addPepper("shared-secret")
+                .with(bcrypt);
+    }
+}
--- src/main/java/com/uon/uonwams/models/User.java
+++ src/main/java/com/uon/uonwams/models/User.java
@@ -1,22 +1,16 @@
 package com.uon.uonwams.models;
 
-import com.password4j.BcryptFunction;
-import com.password4j.Hash;
-import com.password4j.Password;
-import com.password4j.types.Bcrypt;
-
 public class User {
     private int userId;
     private String name;
     private String password;
     private String email;
 
-    private static final BcryptFunction bcrypt = BcryptFunction.getInstance(Bcrypt.B, 12);
-
 //    public static void main(String[] args) {
 //        String pass = hashPassword("12345");
 //        System.out.println(pass);
 //    }
+
 
     public User(int userId, String name, String password, String email) {
         this.userId = userId;
@@ -25,34 +19,28 @@ public class User {
         this.email = email;
     }
 
-    public void login(int userId, String password) {
-
-    }
-
     public void changePassword(int userId, String password) {
-
-    }
-
-    private static String hashPassword(String password) {
-        // Ref: https://davidbertoldi.medium.com/hashing-passwords-in-java-757e787ce71c
-        Hash hash = Password.hash(password)
-                .addPepper("shared-secret")
-                .with(bcrypt);
-        return hash.getResult();
-    }
-
-    private boolean isMatchedPassword(String password) {
-        return Password.check(password, "asdf")
-                .addPepper("shared-secret")
-                .with(bcrypt);
-    }
-
-    public void getUser(int userId, String password) {
 
     }
 
     public void updateUser(int userId, String password) {
 
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     @Override
