64804b7510b3efecb7e463799ffdb6ef7bfaea1d
suthikawong <suthika.wsrd@gmail.com>
2024-12-23 15:17:21 +0000

    add edit profile page

---
+++ src/main/java/com/uon/uonwams/controllers/ProfileFormController.java
@@ -0,0 +1,97 @@
+package com.uon.uonwams.controllers;
+
+import com.uon.uonwams.config.State;
+import com.uon.uonwams.models.User;
+import com.uon.uonwams.models.UserManagement;
+import com.uon.uonwams.models.WAMSApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ProfileFormController extends MenuController implements ControllerInterface {
+    AppController appController;
+
+    @FXML
+    private Label profileFormLabel;
+
+    @FXML
+    private TextField profileFormIdTextField;
+
+    @FXML
+    private TextField profileFormNameTextField;
+
+    @FXML
+    private TextField profileFormEmailTextField;
+
+    @FXML
+    private Label profileFormErrorLabel;
+
+
+    public void setup() {
+        this.setMenuAppController(appController);
+        UserManagement um = new UserManagement(appController.getLoginUser());
+        appController.setUserManagement(um);
+        profileFormErrorLabel.setVisible(false);
+
+        User loginUser = appController.getLoginUser();
+        profileFormLabel.setText("Edit Profile");
+        profileFormIdTextField.setText(Integer.toString(loginUser.getUserId()));
+        profileFormIdTextField.setDisable(true);
+        profileFormNameTextField.setText(loginUser.getName());
+        profileFormEmailTextField.setText(loginUser.getEmail());
+    }
+
+    @Override
+    public void setAppController(AppController appController) {
+        this.appController = appController;
+    }
+
+    @FXML
+    protected void onClickBackButton() throws IOException {
+        appController.setState(State.VIEW_USERS);
+        appController.loadScene("profile-view.fxml");
+    }
+
+    @FXML
+    protected void onClickSaveButton() throws IOException {
+        boolean isValid = validateFields();
+        if (!isValid) return;
+
+        User loginUser = appController.getLoginUser();
+
+        appController.getUserManagement().updateUser(
+                appController.getLoginUser().getUserId(),
+                profileFormNameTextField.getText(),
+                profileFormEmailTextField.getText(),
+                loginUser.getFteRatio(),
+                loginUser.getSubjectArea(),
+                loginUser.getLineManagerUserId()
+        );
+        appController.setState(State.VIEW_PROFILE);
+        appController.loadScene("profile-view.fxml");
+    }
+
+    private boolean validateFields() {
+        boolean isEmpty = checkFieldsEmpty();
+        if (isEmpty) {
+            profileFormErrorLabel.setText("Please fill all required fields");
+            profileFormErrorLabel.setVisible(true);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkFieldsEmpty() {
+        return profileFormIdTextField.getText().isEmpty() ||
+                profileFormNameTextField.getText().isEmpty() ||
+                profileFormEmailTextField.getText().isEmpty();
+    }
+}
--- src/main/java/com/uon/uonwams/controllers/ProfileViewController.java
+++ src/main/java/com/uon/uonwams/controllers/ProfileViewController.java
@@ -3,6 +3,7 @@ package com.uon.uonwams.controllers;
 import com.uon.uonwams.config.State;
 import com.uon.uonwams.data.UserData;
 import com.uon.uonwams.models.User;
+import com.uon.uonwams.models.UserManagement;
 import com.uon.uonwams.models.WAMSApplication;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -46,12 +47,18 @@ public class ProfileViewController extends MenuController implements ControllerI
         profileViewLabel.setText("View Profile");
 
         User loginUser = appController.getLoginUser();
+        User finalLoginUser = loginUser;
+        Optional<User> newLoginUser = WAMSApplication.userData.getUsers().stream().filter(user -> user.getUserId() == finalLoginUser.getUserId()).findFirst();
+        if (newLoginUser.isPresent()) {
+            loginUser = newLoginUser.get();
+            appController.setLoginUser(loginUser);
+        }
         profileViewUserIdLabel.setText(Integer.toString(loginUser.getUserId()));
         profileViewNameLabel.setText(loginUser.getName());
         profileViewEmailLabel.setText(loginUser.getEmail());
         profileViewFteRatioLabel.setText(Double.toString(loginUser.getFteRatio()));
         profileViewSubjectAreaLabel.setText(loginUser.getSubjectArea());
-        Optional<User> lineManagerUser = WAMSApplication.userData.getUsers().stream().filter(user -> ((Integer) user.getUserId()).equals(loginUser.getLineManagerUserId())).findFirst();
+        Optional<User> lineManagerUser = WAMSApplication.userData.getUsers().stream().filter(user -> ((Integer) user.getUserId()).equals(finalLoginUser.getLineManagerUserId())).findFirst();
         profileViewLineManagerLabel.setText(lineManagerUser.isPresent() ? lineManagerUser.get().getName() : "-");
     }
 
